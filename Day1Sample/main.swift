c38cf705267eae8d4455adc713dc62583bc336f6
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-07 20:41:09 -0400

    Day1 1st half lecture

--- Day1Sample/main.swift
+++ Day1Sample/main.swift
@@ -10,3 +10,36 @@ import Foundation
 
 print("Hello, World!")
 
+var str = "Hello, playground"
+//str=100
+print(str)
+var a=100;var b=200
+var c=a+b;
+print ("sum of \(a) and \(b) is \(c)")
+
+var p="😇"
+print(p)
+
+var 🐯="Lion"
+print(🐯)
+var x1=(1,"Pritesh","Patel")
+print(x1)
+print(x1.0)
+print(x1.1)
+print(x1.2)
+
+
+var x2=( 1,"Lambton College","Toronto")
+print(x2)
+
+var x=1...10
+print(x)
+
+for i in 1...10{
+    print("rakesh  \(i)")
+}
+
+
+for j in stride(from: 10, to: 1, by: -1){
+    print("\nrakesh  \(j)")
+}
